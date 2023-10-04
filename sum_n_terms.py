190ef382e3a891d4e5addf87fc7a42b86542938f
Ananya Sinha <ananya.anand12@gmail.com>
2023-10-04 18:17:21 +0530

    adding 1st prac code

---
+++ sum_n_terms.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+class Solution:
+	def seriesSum(self,n):
+	    return int((n*(n+1)/2))
+ 
+ob = Solution()
+ans = ob.seriesSum(2)
+print(ans)
+
+# } Driver Code Ends
